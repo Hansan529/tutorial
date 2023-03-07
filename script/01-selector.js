9fe1535806ec312e362540fb60ed6b61d5fed342
hansan529 <hansan0529@gmail.com>
2023-03-07 10:17:12 +0900

    Update

---
+++ script/01-selector.js
@@ -0,0 +1,8 @@
+// $(document).ready(() => {});
+$(() => {
+  $("*").css("color", "green");
+  $("h1").css("backgroundColor", "pink");
+  $(".textClass").css("color", "red");
+  $("#textId").css("color", "skyblue");
+  $("h2, #textId").css("border", "1px solid #000");
+});
