23452a546839568ff2835a00d6e9f91178f13989
hansan529 <hansan0529@gmail.com>
2023-03-16 12:09:20 +0900

    Update show, hide, toggle
    show: 요소가 보이도록, 크기, 투명도를 조정한다
    hide: 숨긴다
    toggle: show,hide를 반복적으로 실행한다

---
+++ script/38-effectShowHideToggle.js
@@ -0,0 +1,9 @@
+$(".showBtn").on("click", () => {
+  $(".box").show(1000);
+});
+$(".hideBtn").on("click", () => {
+  $(".box").hide(1000);
+});
+$(".toggleBtn").on("click", () => {
+  $(".box").toggle(1000);
+});
