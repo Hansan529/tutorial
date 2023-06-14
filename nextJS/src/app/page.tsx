9f4b88094ab5110cd2f296015038b572e48d5de6
hansan529 <hansan0529@gmail.com>
2023-06-14 16:53:35 +0900

    docs: 컴포넌트 사용

--- nextJS/src/app/page.tsx
+++ nextJS/src/app/page.tsx
@@ -1,8 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import NavBar from "./components/page";
 
 export default function Home() {
-  return "hi";
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
 }
 
 // export default function Home() {
