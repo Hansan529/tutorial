ead92d002cecc7a31dbfe18f275f0fc1a9d4dbe9
hansan529 <hansan0529@gmail.com>
2023-07-24 15:06:34 +0900

    typescript: 튜플

--- typescript/build/index.js
+++ typescript/build/index.js
@@ -1,19 +1,27 @@
 "use strict";
 // type 설정
-const typeString = 'string';
-const typeNumber = 10;
-const typeArray = [];
-const typeArray2 = [1, 2, 3];
-const typeObject = {};
+const typeString = 'string'; // 문자열
+const typeNumber = 10; // 숫자
+const typeArray = []; // 배열
+const typeArray2 = [1, 2, 3]; // 특정 타입의 배열
+const typeObject = {}; // 오브젝트(객체)
 const typeObject2 = { 1: 'fir', 2: 'sec' };
 const typeVoid = function () {
-    console.log('void');
+    console.log('void'); // 반환되는 값이 없는 타입
 };
 const typeNever = function () {
-    throw new Error();
+    throw new Error(); // 절대로 실행되지 않는 타입
 };
 const typeNever2 = function () {
     while (true) {
         //
     }
 };
+const typeNull = null;
+const typeUndefined = undefined;
+// 타입 추론
+const typeReasoning = 1; // const typeReasoning: number = 1
+// 튜플 (Tuple)
+let tuple;
+tuple = [1, 'text'];
+tuple[1].toUpperCase();
--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,18 +1,28 @@
 // type 설정
-const typeString: string = 'string';
-const typeNumber: number = 10;
-const typeArray: [] = [];
-const typeArray2: number[] = [1, 2, 3];
-const typeObject: {} = {};
+const typeString: string = 'string'; // 문자열
+const typeNumber: number = 10; // 숫자
+const typeArray: [] = []; // 배열
+const typeArray2: number[] = [1, 2, 3]; // 특정 타입의 배열
+const typeObject: {} = {}; // 오브젝트(객체)
 const typeObject2: {} = { 1: 'fir', 2: 'sec' };
 const typeVoid = function (): void {
-  console.log('void');
+  console.log('void'); // 반환되는 값이 없는 타입
 };
 const typeNever = function (): never {
-  throw new Error();
+  throw new Error(); // 절대로 실행되지 않는 타입
 };
 const typeNever2 = function (): never {
   while (true) {
     //
   }
 };
+const typeNull: null = null;
+const typeUndefined: undefined = undefined;
+
+// 타입 추론
+const typeReasoning = 1; // const typeReasoning: number = 1
+
+// 튜플 (Tuple)
+let tuple: [number, string];
+tuple = [1, 'text'];
+tuple[1].toUpperCase();
